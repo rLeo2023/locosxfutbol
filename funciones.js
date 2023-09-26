ca27d4c608af75c5ea952ed322c46fb5e54b8e7d
rLeo2023 <140541840+rLeo2023@users.noreply.github.com>
2023-09-26 10:17:59 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -107,7 +107,7 @@ var puntos = `
     <br>
     <p id="hol">Correctas:</p> <p id="aciertos"></p>
     <p id="hol">Incorrectas:</p> <p id="fallos"></p>
-    <img id="image" src="/img/messi.jpg" alt="">
+    <img id="image" src="img/img5.png" alt="">
 
 `;
 
